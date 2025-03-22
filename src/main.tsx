363dc6a614610953d6ca81c025eafea6fbc29313
Dario Jularic <dariojularic0@gmail.com>
2025-03-22 19:48:45 +0100

    Install router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
+import {  StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>404 Not Found</div>,
+    children: [
+      // {path: "products", element: <Products />,}
+      // {path: "cart", element: <Cart />,}
+      // {path: "about", element: <About />,}
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RouterProvider router={router}>
+
     <App />
+    </RouterProvider>
   </StrictMode>,
 )
