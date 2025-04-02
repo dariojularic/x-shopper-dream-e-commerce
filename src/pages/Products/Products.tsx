107dc692384b8383ba54e2b50aa65bcb3d9cd6b3
Dario Jularic <dariojularic0@gmail.com>
2025-04-02 23:21:36 +0200

    Create ProductCard component

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -6,6 +6,24 @@ function Products() {
     <div>
       <h1>Products</h1>
       <Sidebar />
+
+      <div className="products-container">
+        <p>products found</p>
+        <hr />
+        <div>
+          <label htmlFor="sort">Sort by:</label>
+          <select name="sort" id="sort">
+            <option value="">Price (Lowest)</option>
+            <option value="">Price (Highest)</option>
+            <option value="">Name (A-Z)</option>
+            <option value="">Name (Z-A)</option>
+          </select>
+        </div>
+
+        <div className="products-grid">
+          
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/pages/Products/components/ProductCard.tsx
@@ -0,0 +1,15 @@
+import "./ProductCard.css"
+
+function ProductCard() {
+  return (
+    <div className="product-card">
+      <img src="" alt="" />
+      <div>
+        <h3>Armchair</h3>
+        <span>20€</span>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard;
