82ab478a1021a9445ce4a34510ce890734e0a418
Dario Jularic <dariojularic0@gmail.com>
2025-04-03 10:10:41 +0200

    Add style

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -8,20 +8,27 @@ function Products() {
       <Sidebar />
 
       <div className="products-container">
-        <p>products found</p>
-        <hr />
-        <div>
-          <label htmlFor="sort">Sort by:</label>
-          <select name="sort" id="sort">
-            <option value="">Price (Lowest)</option>
-            <option value="">Price (Highest)</option>
-            <option value="">Name (A-Z)</option>
-            <option value="">Name (Z-A)</option>
-          </select>
+        <div className="product-filter-div">
+          <p>products found</p>
+          {/* <div> */}
+            <hr />
+          {/* </div> */}
+          <div>
+            <label htmlFor="sort">Sort by:</label>
+            <select name="sort" id="sort">
+              <option value="">Price (Lowest)</option>
+              <option value="">Price (Highest)</option>
+              <option value="">Name (A-Z)</option>
+              <option value="">Name (Z-A)</option>
+            </select>
+          </div>
         </div>
 
         <div className="products-grid">
-          
+          <br />
+          <br />
+          <br />
+          <br />
         </div>
       </div>
     </div>
