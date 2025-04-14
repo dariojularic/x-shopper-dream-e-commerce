565d44b0ec5d9bcdca195bd26264c28fed9ab849
Dario Jularic <dariojularic0@gmail.com>
2025-04-14 20:30:17 +0200

    Add style

---
+++ src/pages/Home/components/ForthSection.tsx
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+function ForthSection() {
+  return(
+    <section className="section">
+      <div className="container">
+      <div className="featured-headings-container">
+          <h3 className="section-heading-intro">FEATURED PRODUCTS</h3>
+          <h2 className="section-subheading">
+            The art of modern living unlocked.
+          </h2>
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default ForthSection
