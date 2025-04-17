9de8f5da41aa2e66ce45284297e8312e0713c520
Dario Jularic <dariojularic0@gmail.com>
2025-04-17 22:47:41 +0200

    Refactor code

--- src/pages/Home/components/ForthSection.tsx
+++ src/pages/Home/components/ForthSection.tsx
@@ -14,13 +14,52 @@ function ForthSection() {
           <ul className="skills-list">
             <li>
               {/* <div className="skill-container"> */}
-                <div className="skill-icon-container">
-                  <img src="" alt="" />
-                </div>
-                <div className="skill-container">
-                  <h3>Radiance</h3>
-                  <p>Our commitment to providing sparkling solutions ensures every customer enjoys a shining, glowing experience with unmatched quality.</p>
-                </div>
+              <div className="skill-icon-container">
+                <img src="" alt="" />
+              </div>
+
+              <div className="skill-container">
+                <h3>Radiance</h3>
+                <p>
+                  Our commitment to providing sparkling solutions ensures every
+                  customer enjoys a shining, glowing experience with unmatched
+                  quality.
+                </p>
+              </div>
+              {/* </div> */}
+            </li>
+
+            <li>
+              {/* <div className="skill-container"> */}
+              <div className="skill-icon-container">
+                <img src="" alt="" />
+              </div>
+
+              <div className="skill-container">
+                <h3>Clarity</h3>
+                <p>
+                  With clear vision and keen insight, we guide customers down
+                  the path to lasting success with exceptional service and
+                  anticipation.
+                </p>
+              </div>
+              {/* </div> */}
+            </li>
+
+            <li>
+              {/* <div className="skill-container"> */}
+              <div className="skill-icon-container">
+                <img src="" alt="" />
+              </div>
+
+              <div className="skill-container">
+                <h3>Heritage</h3>
+                <p>
+                  As a trusted partner, we offer expert guidance, drawing on our
+                  deep wisdom and wealth of experience to ensure satisfaction
+                  and success.
+                </p>
+              </div>
               {/* </div> */}
             </li>
           </ul>
