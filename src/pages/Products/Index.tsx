879648fe4b3ee24f3c5e29c3f1fbb952b7fb4557
Dario Jularic <dariojularic0@gmail.com>
2025-04-25 15:21:24 +0200

    Add style

--- src/pages/Products/Index.tsx
+++ src/pages/Products/Index.tsx
@@ -1,38 +1,40 @@
-import "./Index.css"
+import "./Index.css";
 import Sidebar from "./components/Sidebar";
 
 function Products() {
   return (
-    <div>
-      <h1>Products</h1>
-      <Sidebar />
+    <section className="section products-section">
+      <div className="container products-container">
+        <h1>Products</h1>
+        <Sidebar />
 
-      <div className="products-container">
-        <div className="product-filter-div">
-          <p>products found</p>
-          {/* <div> */}
-            <hr />
-          {/* </div> */}
-          <div>
-            <label htmlFor="sort">Sort by:</label>
-            <select name="sort" id="sort">
-              <option value="">Price (Lowest)</option>
-              <option value="">Price (Highest)</option>
-              <option value="">Name (A-Z)</option>
-              <option value="">Name (Z-A)</option>
-            </select>
+        <div className="">
+          <div className="product-filter-div">
+            <p className="products-found-paragraph">products found</p>
+            <div className="break-line-container">
+              <hr className="break-line" />
+            </div>
+            <div>
+              <label className="" htmlFor="sort">Sort by:</label>
+              <select name="sort" id="sort">
+                <option value="">Price (Lowest)</option>
+                <option value="">Price (Highest)</option>
+                <option value="">Name (A-Z)</option>
+                <option value="">Name (Z-A)</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="products-grid">
+            <br />
+            <br />
+            <br />
+            <br />
           </div>
         </div>
-
-        <div className="products-grid">
-          <br />
-          <br />
-          <br />
-          <br />
-        </div>
       </div>
-    </div>
-  )
+    </section>
+  );
 }
 
 export default Products;
