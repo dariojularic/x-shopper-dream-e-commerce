e7e9c58aaa3baf8a0b2d1b5bf5649c3a0f2151fa
Dario Jularic <dariojularic0@gmail.com>
2025-04-29 13:59:15 +0200

    Create cart grid

--- src/pages/Cart/Index.tsx
+++ src/pages/Cart/Index.tsx
@@ -1,7 +1,18 @@
 import "./Index.css"
 
 function Cart() {
-  return <h1>Cart</h1>
+  return (
+    <section className="section cart-section">
+      <div className="container cart-container">
+        <div className="cart-grid">
+          <p className="column-header"><span>Item</span> <span>Price</span> <span>Quantity</span> <span>Subtotal</span></p>
+          <ul>
+
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Cart;
